5e228101754ce644f55bd25f0b86fd3af790d373
Parmo7 <73428262+Parmo7@users.noreply.github.com>
2021-03-28 18:52:10 +0200

    GameFinishedScreen refactored

--- core/src/uk/ac/aston/teamproj/game/screens/MultiGameFinishedScreen.java
+++ core/src/uk/ac/aston/teamproj/game/screens/MultiGameFinishedScreen.java
@@ -26,6 +26,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import uk.ac.aston.teamproj.game.MainGame;
 import uk.ac.aston.teamproj.game.tools.SoundManager;
+import uk.ac.aston.teamproj.superclass.GameFinishedScreen;
 
 /**
  * @author Suleman, Junaid and Marcus
@@ -33,172 +34,34 @@ import uk.ac.aston.teamproj.game.tools.SoundManager;
  * @date 15/03/2021
  */
 
-public class MultiGameFinishedScreen implements Screen {
-
-	private Viewport viewport;
-	private Stage stage;
-	private Skin skin; // skin for buttons
-	private TextureAtlas buttonsAtlasBack; // the sprite-sheet containing all buttons
-	private ImageButton[] optionButtons;
-	@SuppressWarnings("unused")
-	private MainGame game;
-
-	// font
-	private BitmapFont font;
+public class MultiGameFinishedScreen extends GameFinishedScreen {
+	
 	private Label winnerLabel;
 
 	public MultiGameFinishedScreen(MainGame game) {
-		
-		// font
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/RetroGaming.ttf"));
-		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
-		parameter.size = 20;
-		parameter.characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!'()>?:";
-		// e.g. abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!'()>?:
-		// These characters should not repeat!
-
-		font = generator.generateFont(parameter);
-		font.setColor(Color.WHITE);
-		generator.dispose();
-		
-		this.game = game;
-		viewport = new FitViewport(MainGame.V_WIDTH / 6, MainGame.V_HEIGHT / 6, new OrthographicCamera());
-		stage = new Stage(viewport, ((MainGame) game).batch);
-
-		buttonsAtlasBack = new TextureAtlas("buttons/new_buttons.pack");
-		skin = new Skin(buttonsAtlasBack);
-		optionButtons = new ImageButton[1];
-
-		initializeButtons();
-		populateTable();
-		getWinner();
-		System.out.println(getWinner());
+		super(game);
 		PlayScreen.resetSession();
 	}
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+	public void render(float delta) {
+		super.render(delta);
+		winnerLabel.setText(PlayScreen.winner);	
 	}
 
-	private void initializeButtons() {
-		ImageButtonStyle style;
-
-		// Go Back Button
-		style = new ImageButtonStyle();
-		style.up = skin.getDrawable("back_inactive"); // set default image
-		style.over = skin.getDrawable("back_active"); // set image for mouse over
-		ImageButton backBtn = new ImageButton(style);
-		backBtn.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-
-				SoundManager.playSound(SoundManager.POP);
-				System.out.println("Back");
-				MultiGameFinishedScreen.this.dispose();
-				game.setScreen(new MainMenuScreen(game));
-				return true;
-			}
-		});
-
-		optionButtons[0] = backBtn;
-
-	}
 
 	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		winnerLabel.setText(getWinner());
-		
-		stage.draw();
-		stage.act(delta);	
-	}
-
-	public void populateTable() {
-		
-		Table LabelWinnerIsTable = new Table();
-		LabelWinnerIsTable.top();
-		LabelWinnerIsTable.setFillParent(true);
-		
-		Table winnerNameTable = new Table();
-		winnerNameTable.top();
-		winnerNameTable.setFillParent(true);
-		
-		//LabelName
-        Label.LabelStyle labelName = new Label.LabelStyle(font, Color.WHITE);
-        winnerLabel = new Label (getWinner(), labelName);
-        winnerNameTable.add(winnerLabel).expandX().padTop(100).padLeft(150);
-        
+	protected void addLabelWinnerIs() {
 		//LabelWinner
         Label.LabelStyle label = new Label.LabelStyle(font, Color.WHITE);
         Label gameOverLabel = new Label ("Winner:   ", label);
-        LabelWinnerIsTable.add(gameOverLabel).expandX().padLeft(180).padTop(50);
-		
-		// Set background textures
-		Texture background = new Texture("buttons/Sky.png");
-		LabelWinnerIsTable.background(new TextureRegionDrawable(new TextureRegion(background)));
-		LabelWinnerIsTable.row();
-		
-		Table buttonTable = new Table();
-		buttonTable.top();
-		buttonTable.setFillParent(true);
-		
-		// backBtn
-		ImageButton backBtn = optionButtons[0];
-		buttonTable.add(backBtn).height(22f).width(120).padTop(175).padRight(240);
-		buttonTable.row();
-		
-		// Adding table to screen
-		stage.addActor(LabelWinnerIsTable);
-		stage.addActor(winnerNameTable);
-		stage.addActor(buttonTable);
-		Gdx.input.setInputProcessor(stage);
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height, true);
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-
+        winnerLabelTable.add(gameOverLabel).expandX().padLeft(180).padTop(50);
 	}
 	
-	public String getWinner() {
-		return PlayScreen.winner;
-	}
-	
-//	public String getWinner() {
-//		for(int i = 0; i < PlayScreen.players.size(); i++ ) {
-//			if(PlayScreen.players.get(i).getWonState()) {
-//				winnerName = PlayScreen.players.get(i).getName();
-//			}
-//		}
-//		return winnerName;
-//	}
-
 	@Override
-	public void dispose() {
-		stage.dispose();
-		skin.dispose();
-		buttonsAtlasBack.dispose();
+	protected void addLabelName() {
+		Label.LabelStyle labelName = new Label.LabelStyle(font, Color.WHITE);
+        winnerLabel = new Label (PlayScreen.winner, labelName);
+        winnerNameTable.add(winnerLabel).expandX().padTop(100).padLeft(150);
 	}
-	
-
 }
