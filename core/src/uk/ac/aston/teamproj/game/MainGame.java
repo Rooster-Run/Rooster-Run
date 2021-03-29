246e92fee2b352be039ddda89634b68531ff9d1b
suleman2020 <190028486@aston.ac.uk>
2021-03-29 14:36:35 +0100

    update maingame.java
    IP ADDRESS

--- core/src/uk/ac/aston/teamproj/game/MainGame.java
+++ core/src/uk/ac/aston/teamproj/game/MainGame.java
@@ -33,7 +33,7 @@ public class MainGame extends Game {
 	public static final short GROUND_BIT = 2048;
 
 //	public static final String IP = "35.246.103.114";
-	public static final String IP = "localhost";
+	public static final String IP = "35.214.16.148";
 
 	@Override
 	public void create () {
